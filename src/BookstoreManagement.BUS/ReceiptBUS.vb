ca10e2c61430060c0a8f0f33599c9be852c70009
Hồng Gia Lập <honggialap@gmail.com>
2018-06-29 20:04:53 +0700

    Updating BUS

--- src/BookstoreManagement.BUS/ReceiptBUS.vb
+++ src/BookstoreManagement.BUS/ReceiptBUS.vb
@@ -21,7 +21,11 @@ Public Class ReceiptBUS
 		Return receiptDAL.insert(receipt)
 	End Function
 
-	Public Function selectAll(ByRef receipts As List(Of ReceiptDTO)) As Result
+	Public Function select_ByID(receiptID As String, ByRef receipt As ReceiptDTO) As Result
+		Return receiptDAL.select_ByID(receiptID, receipt)
+	End Function
+
+	Public Function selectALL(ByRef receipts As List(Of ReceiptDTO)) As Result
 		Return receiptDAL.selectAll(receipts)
 	End Function
 
